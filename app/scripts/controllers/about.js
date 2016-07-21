13407ae0599366fe008924fc2bab0ba1caa43409
stevensotelo <stevenbetancurt@hotmail.com>
2016-07-21 07:55:15 -0500

    init project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cosechaPatronesClimaticosApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cosechaPatronesClimaticosApp
+ */
+angular.module('cosechaPatronesClimaticosApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
